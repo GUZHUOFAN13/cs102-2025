396c2d744b668f65af2f281a3ae0b0beb36ca3de
GUZHUOFAN13 <3190076025@qq.com>
2025-12-27 14:58:05 +0800

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,16 +1,26 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+    """
+    Encrypts plaintext using a Vigenere cipher.
+
+    >>> encrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> encrypt_vigenere("python", "a")
+    'python'
+    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+    'LXFOPVEFRNHR'
+    """
     ciphertext = ""
     key_index = 0
     keyword = keyword.lower()
 
     for ch in plaintext:
-        if 'a' <= ch <= 'z':
-            shift = ord(keyword[key_index % len(keyword)]) - ord('a')
-            ciphertext += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+        if "a" <= ch <= "z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            ciphertext += chr(((ord(ch) - ord("a") + shift) % 26) + ord("a"))
             key_index += 1
-        elif 'A' <= ch <= 'Z':
-            shift = ord(keyword[key_index % len(keyword)]) - ord('a')
-            ciphertext += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
+        elif "A" <= ch <= "Z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            ciphertext += chr(((ord(ch) - ord("A") + shift) % 26) + ord("A"))
             key_index += 1
         else:
             ciphertext += ch
@@ -18,22 +28,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     return ciphertext
 
 
-
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+    """
+    Decrypts a ciphertext using a Vigenere cipher.
+
+    >>> decrypt_vigenere("PYTHON", "A")
+    'PYTHON'
+    >>> decrypt_vigenere("python", "a")
+    'python'
+    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+    'ATTACKATDAWN'
+    """
     plaintext = ""
     key_index = 0
     keyword = keyword.lower()
 
     for ch in ciphertext:
-        if 'a' <= ch <= 'z':
-            shift = ord(keyword[key_index % len(keyword)]) - ord('a')
-            plaintext += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+        if "a" <= ch <= "z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            plaintext += chr(((ord(ch) - ord("a") - shift) % 26) + ord("a"))
             key_index += 1
-        elif 'A' <= ch <= 'Z':
-            shift = ord(keyword[key_index % len(keyword)]) - ord('a')
-            plaintext += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+        elif "A" <= ch <= "Z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            plaintext += chr(((ord(ch) - ord("A") - shift) % 26) + ord("A"))
             key_index += 1
         else:
             plaintext += ch
 
     return plaintext
+
