23733f4ea25829f2b9c9fe88dcfbd5b0b565994f
GUZHUOFAN13 <3190076025@qq.com>
2025-12-27 13:56:18 +0800

    Refactor hello_world.py to simplify text function
    Removed unnecessary comments and simplified the text function.

--- homework00/hello_world.py
+++ homework00/hello_world.py
@@ -1,12 +1,2 @@
-"""
-homework00 中的 hello_world 模块，用于输出 Hello, World!
-"""
-
-
 def text():
-    """
-    生成并返回 Hello, World! 消息
-    :return: 包含 "Hello, World!" 的字符串
-    """
-    message = "Hello, World!"
-    return message
+    return "Hello, World!"
