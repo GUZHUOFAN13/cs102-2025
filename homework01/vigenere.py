97be532750a67829dc273b320779bf8513fd9f10
GUZHUOFAN13 <3190076025@qq.com>
2025-12-27 15:07:22 +0800

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,6 +1,7 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
+
     >>> encrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> encrypt_vigenere("python", "a")
@@ -9,13 +10,28 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    key_index = 0
+    keyword = keyword.lower()
+
+    for ch in plaintext:
+        if "a" <= ch <= "z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            ciphertext += chr((ord(ch) - ord("a") + shift) % 26 + ord("a"))
+            key_index += 1
+        elif "A" <= ch <= "Z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            ciphertext += chr((ord(ch) - ord("A") + shift) % 26 + ord("A"))
+            key_index += 1
+        else:
+            ciphertext += ch
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
+
     >>> decrypt_vigenere("PYTHON", "A")
     'PYTHON'
     >>> decrypt_vigenere("python", "a")
@@ -24,5 +40,19 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    key_index = 0
+    keyword = keyword.lower()
+
+    for ch in ciphertext:
+        if "a" <= ch <= "z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            plaintext += chr((ord(ch) - ord("a") - shift) % 26 + ord("a"))
+            key_index += 1
+        elif "A" <= ch <= "Z":
+            shift = ord(keyword[key_index % len(keyword)]) - ord("a")
+            plaintext += chr((ord(ch) - ord("A") - shift) % 26 + ord("A"))
+            key_index += 1
+        else:
+            plaintext += ch
+
     return plaintext
