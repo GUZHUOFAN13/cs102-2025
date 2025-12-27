3fefa69bbb1f053707ccc2c290a07a8821302e84
GUZHUOFAN13 <3190076025@qq.com>
2025-12-27 14:59:17 +0800

    Update caesar.py

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,20 +12,22 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
-    for ch in plaintext:
-    if 'a' <= ch <= 'z':
-        ciphertext += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
-    elif 'A' <= ch <= 'Z':
-        ciphertext += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
-    else:
-        ciphertext += ch
 
+    for ch in plaintext:
+        if "a" <= ch <= "z":
+            ciphertext += chr(((ord(ch) - ord("a") + shift) % 26) + ord("a"))
+        elif "A" <= ch <= "Z":
+            ciphertext += chr(((ord(ch) - ord("A") + shift) % 26) + ord("A"))
+        else:
+            ciphertext += ch
+
+    return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -35,13 +38,15 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
+
     for ch in ciphertext:
-    if 'a' <= ch <= 'z':
-        plaintext += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
-    elif 'A' <= ch <= 'Z':
-        plaintext += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
-    else:
-        plaintext += ch
-    
-return plaintext
+        if "a" <= ch <= "z":
+            plaintext += chr(((ord(ch) - ord("a") - shift) % 26) + ord("a"))
+        elif "A" <= ch <= "Z":
+            plaintext += chr(((ord(ch) - ord("A") - shift) % 26) + ord("A"))
+        else:
+            plaintext += ch
+
+    return plaintext
+
 
