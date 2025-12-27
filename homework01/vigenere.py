3c6bc97c5a4aa10495599e789412d9e23cede334
GUZHUOFAN13 <3190076025@qq.com>
2025-12-27 16:03:29 +0800

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -16,11 +16,15 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for ch in plaintext:
         if "a" <= ch <= "z":
             shift = ord(keyword[key_index % len(keyword)]) - ord("a")
-            ciphertext += chr(((ord(ch) - ord("a") + shift) % 26) + ord("a"))
+            ciphertext += chr(
+                ((ord(ch) - ord("a") + shift) % 26) + ord("a")
+            )
             key_index += 1
         elif "A" <= ch <= "Z":
             shift = ord(keyword[key_index % len(keyword)]) - ord("a")
-            ciphertext += chr(((ord(ch) - ord("A") + shift) % 26) + ord("A"))
+            ciphertext += chr(
+                ((ord(ch) - ord("A") + shift) % 26) + ord("A")
+            )
             key_index += 1
         else:
             ciphertext += ch
@@ -46,14 +50,19 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     for ch in ciphertext:
         if "a" <= ch <= "z":
             shift = ord(keyword[key_index % len(keyword)]) - ord("a")
-            plaintext += chr(((ord(ch) - ord("a") - shift) % 26) + ord("a"))
+            plaintext += chr(
+                ((ord(ch) - ord("a") - shift) % 26) + ord("a")
+            )
             key_index += 1
         elif "A" <= ch <= "Z":
             shift = ord(keyword[key_index % len(keyword)]) - ord("a")
-            plaintext += chr(((ord(ch) - ord("A") - shift) % 26) + ord("A"))
+            plaintext += chr(
+                ((ord(ch) - ord("A") - shift) % 26) + ord("A")
+            )
             key_index += 1
         else:
             plaintext += ch
 
     return plaintext
+
 
