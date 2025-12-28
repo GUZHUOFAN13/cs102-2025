217814928f549713a33b49ada6237d8e8cf4f44a
GUZHUOFAN13 <3190076025@qq.com>
2025-12-28 09:41:25 +0800

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,18 +12,12 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
+    # PUT YOUR CODE HERE
     if n <= 1:
         return False
-    if n <= 3:
-        return True
-    if n % 2 == 0:
-        return False
-
-    i = 3
-    while i * i <= n:
+    for i in range(2, n):
         if n % i == 0:
             return False
-        i += 2
     return True
 
 
@@ -35,6 +29,7 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
+    # PUT YOUR CODE HERE
     while b != 0:
         a, b = b, a % b
     return a
@@ -47,16 +42,11 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
-    old_r, r = e, phi
-    old_s, s = 1, 0
-
-    while r != 0:
-        q = old_r // r
-        old_r, r = r, old_r - q * r
-        old_s, s = s, old_s - q * s
-
-    # old_r is gcd(e, phi) and should be 1
-    return old_s % phi
+    # PUT YOUR CODE HERE
+    d = 1
+    while (d * e) % phi != 1:
+        d += 1
+    return d
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -66,9 +56,11 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
+    # PUT YOUR CODE HERE
     n = p * q
 
     # phi = (p-1)(q-1)
+    # PUT YOUR CODE HERE
     phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
@@ -86,3 +78,38 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
     # Return public and private keypair
     # Public key is (e, n) and private key is (d, n)
     return ((e, n), (d, n))
+
+
+def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
+    # Unpack the key into it's components
+    key, n = pk
+    # Convert each letter in the plaintext to numbers based on
+    # the character using a^b mod m
+    cipher = [(ord(char) ** key) % n for char in plaintext]
+    # Return the array of bytes
+    return cipher
+
+
+def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
+    # Unpack the key into its components
+    key, n = pk
+    # Generate the plaintext based on the ciphertext and key using a^b mod m
+    plain = [chr((char ** key) % n) for char in ciphertext]
+    # Return the array of bytes as a string
+    return "".join(plain)
+
+
+if __name__ == "__main__":
+    print("RSA Encrypter/ Decrypter")
+    p = int(input("Enter a prime number (17, 19, 23, etc): "))
+    q = int(input("Enter another prime number (Not one you entered above): "))
+    print("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print("Your public key is ", public, " and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print("Your encrypted message is: ")
+    print("".join(map(lambda x: str(x), encrypted_msg)))
+    print("Decrypting message with public key ", public, " . . .")
+    print("Your message is:")
+    print(decrypt(public, encrypted_msg))
