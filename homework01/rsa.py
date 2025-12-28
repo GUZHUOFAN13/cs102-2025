cca801369b032472724a2d029de56f8d6648c9b6
GUZHUOFAN13 <3190076025@qq.com>
2025-12-28 09:23:42 +0800

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -4,6 +4,7 @@ import typing as tp
 
 def is_prime(n: int) -> bool:
     """
+    Tests to see if a number is prime.
     >>> is_prime(2)
     True
     >>> is_prime(11)
@@ -13,16 +14,22 @@ def is_prime(n: int) -> bool:
     """
     if n <= 1:
         return False
+    if n <= 3:
+        return True
+    if n % 2 == 0:
+        return False
 
-    for i in range(2, n):
+    i = 3
+    while i * i <= n:
         if n % i == 0:
             return False
-
+        i += 2
     return True
 
 
 def gcd(a: int, b: int) -> int:
     """
+    Euclid's algorithm for determining the greatest common divisor.
     >>> gcd(12, 15)
     3
     >>> gcd(3, 7)
@@ -30,49 +37,52 @@ def gcd(a: int, b: int) -> int:
     """
     while b != 0:
         a, b = b, a % b
-
     return a
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
     """
+    Euclid's extended algorithm for finding the multiplicative
+    inverse of two numbers.
     >>> multiplicative_inverse(7, 40)
     23
     """
-    d = 1
-    while (d * e) % phi != 1:
-        d += 1
+    old_r, r = e, phi
+    old_s, s = 1, 0
 
-    return d
+    while r != 0:
+        q = old_r // r
+        old_r, r = r, old_r - q * r
+        old_s, s = s, old_s - q * s
+
+    # old_r is gcd(e, phi) and should be 1
+    return old_s % phi
 
 
-def generate_keypair(
-    p: int, q: int
-) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
+def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
         raise ValueError("Both numbers must be prime.")
     elif p == q:
         raise ValueError("p and q cannot be equal")
 
+    # n = pq
     n = p * q
+
+    # phi = (p-1)(q-1)
     phi = (p - 1) * (q - 1)
 
+    # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
-    while gcd(e, phi) != 1:
-        e = random.randrange(1, phi)
 
+    # Use Euclid's Algorithm to verify that e and phi(n) are coprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    # Use Extended Euclid's Algorithm to generate the private key
     d = multiplicative_inverse(e, phi)
 
-    return (e, n), (d, n)
-
-
-def encrypt(pk: tp.Tuple[int, int], plaintext: str) -> tp.List[int]:
-    key, n = pk
-    return [(ord(char) ** key) % n for char in plaintext]
-
-
-def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
-    key, n = pk
-    return "".join(chr((char ** key) % n) for char in ciphertext)
-
-
+    # Return public and private keypair
+    # Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
