e329786240087b180b9521156355c9351cdb8cd0
Your Name <you@example.com>
2026-01-10 07:20:45 +0800

    Finish homework01

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,22 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    for ch in plaintext:
+        if 'a' <= ch <= 'z':
+            ciphertext += chr((ord(ch) - ord('a') + shift) % 26 + ord('a'))
+        elif 'A' <= ch <= 'Z':
+            ciphertext += chr((ord(ch) - ord('A') + shift) % 26 + ord('A'))
+        else:
+            ciphertext += ch
+
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +38,13 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    for ch in ciphertext:
+        if 'a' <= ch <= 'z':
+            plaintext += chr((ord(ch) - ord('a') - shift) % 26 + ord('a'))
+        elif 'A' <= ch <= 'Z':
+            plaintext += chr((ord(ch) - ord('A') - shift) % 26 + ord('A'))
+        else:
+            plaintext += ch
+
     return plaintext
