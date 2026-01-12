5ef6f47053658d00dc0fa9f881c0c723aebaa333
GUZHUOFAN13 <3190076025@qq.com>
2026-01-12 11:41:55 +0800

    Update maze.py

--- homework03/maze.py
+++ homework03/maze.py
@@ -60,9 +60,7 @@ def bin_tree_maze(rows: int = 15, cols: int = 15, random_exit: bool = True) -> G
     if random_exit:
         x_in, x_out = randint(0, rows - 1), randint(0, rows - 1)
         y_in = randint(0, cols - 1) if x_in in (0, rows - 1) else choice((0, cols - 1))
-        y_out = (
-            randint(0, cols - 1) if x_out in (0, rows - 1) else choice((0, cols - 1))
-        )
+        y_out = randint(0, cols - 1) if x_out in (0, rows - 1) else choice((0, cols - 1))
     else:
         x_in, y_in = 0, cols - 2
         x_out, y_out = rows - 1, 1
@@ -150,9 +148,7 @@ def shortest_path(grid: Grid, exit_coord: Coord) -> Optional[Union[Coord, List[C
 
         for di, dj in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             ni, nj = i + di, j + dj
-            if 0 <= ni < rows and 0 <= nj < cols and grid[ni][nj] == next_val:
-                next_cell = (ni, nj)
-                break
+            if 0 <= ni < len(work) and 0 <= nj < len(work[0]) and isinstance(work[ni][nj], int):
 
         if next_cell is None:
             return None
